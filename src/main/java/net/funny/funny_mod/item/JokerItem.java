10c73615396051b3bd8bd56b7ee1301d6afe748a
Wmy20130327 <Wmy20130327@outlook.com>
2026-08-29 15:37:37 +0800

    Initial commit

---
+++ src/main/java/net/funny/funny_mod/item/JokerItem.java
@@ -0,0 +1,22 @@
+package net.funny.funny_mod.item;
+
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.World;
+
+public class JokerItem extends Item {
+
+    public JokerItem(Settings settings) {
+        super(settings);
+    }
+
+    public
+
+}
